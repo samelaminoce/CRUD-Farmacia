2f5126ebb30981c1fc28934954259f36904029f6
samelaminoce <samelasocial.md@gmail.com>
2024-01-31 13:31:11 -0300

    CRUD-Categoria

---
+++ src/main/java/com/generation/farmacia/model/Categoria.java
@@ -0,0 +1,67 @@
+package com.generation.farmacia.model;
+
+
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table (name ="tb_categoria")
+public class Categoria {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+    @NotBlank(message = "O atributo título é Obrigatório!")
+    @Size(max = 255, message = "O atributo tipo deve conter no máximo 255 caracteres")
+	private String tipo;
+	
+    @NotBlank(message = "O atributo Descrição é Obrigatório!")
+    @Size(min = 10, max = 255, message = "O atributo Descrição deve conter no mínimo 10 e no máximo 255 caracteres")
+    private String descricao;
+    
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "categoria", cascade = CascadeType.REMOVE)
+    @JsonIgnoreProperties("categoria")
+  
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+
+
+	}
+
+
+
